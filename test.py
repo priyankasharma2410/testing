32f5c440f93c73d27c4edf59a67a87b10eb84d75
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-03 23:25:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,46 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 # Set the path to the ChromeDriver executable
 chrome_driver_path = 'C:\\Users\\91881\\Desktop\\sih\\product development\\chromedriver-win64\\chromedriver.exe'
+
 # Initialize the Chrome WebDriver
-#driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-# Define the base URL
-base_url = "https://www.dominos.co.in"
+# Open the chatbot webpage
+base_url = "https://khaanvaani.streamlit.app/"  # Replace with the URL of the chatbot
 
-# Open the website
-#webdriver.get(base_url)
+# Navigate to the chatbot's webpage
+driver.get(base_url)
 
-# Find and click on an element using its XPath
-webdriver.find_element(By.XPATH, "//*[@id='ymPluginDivContainerInitial']//img").click()
+# Find the chat input field by class name
+chat_input = driver.find_element_by_class_name('st-c0')
 
-# Create a WebDriverWait instance
-wait = WebDriverWait(driver, 60)
+# Start a conversation
+chat_input.send_keys("How can I assist you?")
+chat_input.send_keys(Keys.RETURN)
 
-# Wait for the presence of an element
-wait.until(EC.presence_of_element_located((By.XPATH, "//strong[contains(text(),'Stores near me')]")))
+# Wait for the chatbot's response (you may need to adjust the wait time)
+driver.implicitly_wait(5)
 
-# Find and click on the element
-webdriver.find_element(By.XPATH, "//strong[contains(text(),'Stores near me')]").click()
+# Capture the chatbot's response by finding a message element
+response = driver.find_element_by_id("st-dc")
+print("Chatbot: " + response.text)
 
-print("Found the button, clicked on that...")
+# Continue the conversation
+chat_input.clear()
+chat_input.send_keys("I had an accident in a mining site. What should I do?")
+chat_input.send_keys(Keys.RETURN)
 
-# Close the browser
-webdriver.close()
+# Wait for the chatbot's response (adjust the wait time)
+driver.implicitly_wait(5)
 
-# Exit the script
-webdriver.quit()
+# Capture the chatbot's response
+response = driver.find_element_by_id("st-dc")
+print("Chatbot: " + response.text)
+
+# You can continue the conversation by repeating the steps above
+# ...
+
+# Close the browser when done
+driver.quit()
