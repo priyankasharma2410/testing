7973b86c820e9e9fff0e0dae815061955f4ee38c
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-03 22:43:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,10 +12,10 @@ chrome_driver_path = 'C:\\Users\\91881\\Desktop\\sih\\product development\\chrom
 base_url = "https://www.dominos.co.in"
 
 # Open the website
-#driver.get(base_url)
+webdriver.get(base_url)
 
 # Find and click on an element using its XPath
-driver.find_element(By.XPATH, "//*[@id='ymPluginDivContainerInitial']//img").click()
+webdriver.find_element(By.XPATH, "//*[@id='ymPluginDivContainerInitial']//img").click()
 
 # Create a WebDriverWait instance
 wait = WebDriverWait(driver, 60)
@@ -24,12 +24,12 @@ wait = WebDriverWait(driver, 60)
 wait.until(EC.presence_of_element_located((By.XPATH, "//strong[contains(text(),'Stores near me')]")))
 
 # Find and click on the element
-driver.find_element(By.XPATH, "//strong[contains(text(),'Stores near me')]").click()
+webdriver.find_element(By.XPATH, "//strong[contains(text(),'Stores near me')]").click()
 
 print("Found the button, clicked on that...")
 
 # Close the browser
-driver.close()
+webdriver.close()
 
 # Exit the script
-driver.quit()
+webdriver.quit()
