ddee953a4d3eb9d469632ab99c0cfdf2e75db2b4
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-03 23:13:45 +0530

    Update ntest.py

--- ntest.py
+++ ntest.py
@@ -8,17 +8,17 @@ driver = "C:\\Users\\91881\\Desktop\\sih\\product development\\chromedriver-win6
 base_url ="https://khaanvaani.streamlit.app//"  # Replace with the URL of the chatbot
 
 # Find the chat input field by tag name (input)
-chat_input = driver.find_element_by_tag_name("input")
+chat_input = webdriver.find_element_by_tag_name("input")
 
 # Start a conversation
 chat_input.send_keys("How can I assist you?")
 chat_input.send_keys(Keys.RETURN)
 
 # Wait for the chatbot's response (you may need to adjust the wait time)
-driver.implicitly_wait(5)
+webdriver.implicitly_wait(5)
 
 # Capture the chatbot's response by finding a message element
-response = driver.find_element_by_id("st-dc")
+response = webdriver.find_element_by_id("st-dc")
 print("Chatbot: " + response.text)
 
 # Continue the conversation
@@ -27,10 +27,10 @@ chat_input.send_keys("I had an accident in a mining site. What should I do?")
 chat_input.send_keys(Keys.RETURN)
 
 # Wait for the chatbot's response (adjust the wait time)
-driver.implicitly_wait(5)
+webdriver.implicitly_wait(5)
 
 # Capture the chatbot's response
-response = driver.find_element_by_id("st-dc")
+response = webdriver.find_element_by_id("st-dc")
 print("Chatbot: " + response.text)
 
 # You can continue the conversation by repeating the steps above
