d5162f0f580d570b60d6b9bab029fa9d21c22c77
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-03 22:33:20 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-from selenium import webdriver
+import org.openqa.selenium.By;
 
-# Initialize the WebDriver
-driver = webdriver.Chrome()  # or webdriver.Firefox() for Firefox
+import org.openqa.selenium.WebDriver;
 
-# Navigate to a website
-driver.get("https://khaanvaani.streamlit.app/")
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Homepage {
+
+public static void main(String[] args) {
+
+System.setProperty("webdriver.chrome.driver","");
+
+WebDriver driver = new ChromeDriver();
+
+String baseUrl = "”Domino’s Pizza – Order Online | Get 2 Regular Pizza @99 Each (dominos.co.in);
+
+driver.get(baseUrl);
+
+driver.findElement(By.xpath("////body/div[@id='ymPluginDivContainerInitial']/div[@id='ymDivCircle']/img[1]")).click();
+
+WebDriverWait wait = new WebDriverWait(driver,60) ;
+
+wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//strong[contains(text(),'Stores near me')]")));
+
+driver.findElement(By.xpath("//strong[contains(text(),'Stores near me')]")).click();
+
+System.out.println("Found the button, clicked on that...");
+
+driver.close();
+
+System.exit(0);
+
+}
+
+}
