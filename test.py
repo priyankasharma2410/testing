f7213d91ebc4ce56a179f33c17369168f8ce91ca
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-04 19:59:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# Initialize the WebDriver (e.g., Chrome WebDriver)
-driver = "C:\\Users\\91881\\Desktop\\sih\\product development\\chromedriver-win64\\chromedriver.exe"
+# Initialize the Chrome WebDriver
+driver = webdriver.Chrome(executable_path="C:\\Users\\91881\\Desktop\\sih\\product development\\chromedriver-win64\\chromedriver.exe")
 
 # Navigate to a website
 webpage_url = "https://khaanvaani.streamlit.app/"
+driver.get(webpage_url)
 
 # Find the first <input> element on the page by tag name
-chat_input  = "C:\\Users\\91881\\Desktop\\sih\\product development\\chromedriver-win64\\chromedriver.exe"
+chat_input = driver.find_element(By.TAG_NAME, "input")
 
 # Perform actions on the input element
 chat_input.send_keys("Your message")  # You can send text to the input element
