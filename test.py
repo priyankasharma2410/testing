0c0ef693b3f4b28e895811ec91dc0432652c5e51
priyankasharma2410 <141417152+priyankasharma2410@users.noreply.github.com>
2023-11-19 20:31:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,26 +6,23 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.common.exceptions import WebDriverException
 
 try:
-    # Your existing code herewith webdriver.Firefox() as driver:
     with webdriver.Firefox() as driver:
-    driver.get("http://google.com/ncr")
-    
-    # Locate the search input field and send the search query
-    search_input = driver.find_element(By.NAME, "q")
-    search_input.send_keys("cheese" + Keys.RETURN)
-    
-    # Use WebDriverWait for waiting for the presence of the search results container
-    wait = WebDriverWait(driver, 10)
-    wait.until(presence_of_element_located((By.XPATH, '//div[@id="rcnt"]')))
-    
-    # Find all the search result links using a more specific XPath
-    results = driver.find_elements(By.XPATH, '//div[@class="tF2Cxc"]/div/a[@href]')
-    
-    # Print the search results
-    for i, elem in enumerate(results):
-        print(f'#{i + 1} {elem.text} ({elem.get_attribute("href")})')
+        driver.get("http://google.com/ncr")
+        
+        # Locate the search input field and send the search query
+        search_input = driver.find_element(By.NAME, "q")
+        search_input.send_keys("cheese" + Keys.RETURN)
+        
+        # Use WebDriverWait for waiting for the presence of the search results container
+        wait = WebDriverWait(driver, 10)
+        wait.until(presence_of_element_located((By.XPATH, '//div[@id="rcnt"]')))
+        
+        # Find all the search result links using a more specific XPath
+        results = driver.find_elements(By.XPATH, '//div[@class="tF2Cxc"]/div/a[@href]')
+        
+        # Print the search results
+        for i, elem in enumerate(results):
+            print(f'#{i + 1} {elem.text} ({elem.get_attribute("href")})')
 
 except WebDriverException as e:
     print(f"WebDriverException: {e}")
-
-
